43fec1363686349bbc509023ac28b1a3c4fb7bd1
Guilherme Vieira <gvmmpg@gmail.com>
2021-06-29 14:57:52 -0300

    change url path

--- src/main/java/one/digitalinnovation/registerapi/controller/RegisterController.java
+++ src/main/java/one/digitalinnovation/registerapi/controller/RegisterController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/talia")
+@RequestMapping("/registerapi")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class RegisterController {
 
